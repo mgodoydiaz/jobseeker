2151ec3a841d51495b49328cf4684ad3b4213436
magodoydiaz <magodoydiaz@gmail.com>
2025-12-30 23:51:29 +0000

    feat(frontend): Create Home view with sidebar and project description

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Ofertas from './views/Ofertas/Ofertas';
+import Home from './views/Home/Home';
 import './assets/css/App.css';
 import './assets/css/index.css';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Ofertas />} />
+        <Route path="/" element={<Home />} />
         <Route path="/popup" element={<App />} />
       </Routes>
     </HashRouter>
