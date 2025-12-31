629fa05f330a5437251a6b79a961f3801bef1fb6
Miguel Godoy <magodoydiaz@gmail.com>
2025-12-31 13:11:24 -0300

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,8 @@ export default function App() {
   return (
     <Router>
       <Layout>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* Otras rutas pueden ir aquí */}
-        </Routes>
+        <h1> Bienvenido a Job Seeker Assistant</h1>
+        <p>Job Seeker Assistant es una herramienta diseñada para simplificar y organizar la búsqueda de empleo. Permite a los usuarios guardar, gestionar y analizar ofertas de trabajo de manera eficiente.</p>
       </Layout>
     </Router>
   );
